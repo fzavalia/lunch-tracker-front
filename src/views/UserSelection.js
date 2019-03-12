e9be235377b5b949415b09bfdda6613cdb17c243
Fernando Zavalia <zavaliafernando@gmail.com>
2019-03-12 00:52:15 -0300

    Reformat code

--- src/views/UserSelection.js
+++ src/views/UserSelection.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Button, Select, MenuItem, FormControl, InputLabel, Typography } from '@material-ui/core'
 import api from '../api';
-import { setCurrentUser } from '../hooks/useCurrentUser';
+import useCurrentUser, { setCurrentUser } from '../hooks/useCurrentUser';
 
 const useUsers = (page, perPage) => {
 
@@ -15,35 +15,61 @@ const useUsers = (page, perPage) => {
   return users
 }
 
-const UserSelectionContainer = ({ history }) =>
-  <UserSelection
-    onUserSelected={user => {
-      if (user) {
-        history.push('/main')
-        setCurrentUser(user)
-      }
-    }}
-    onCreateUser={() => {
-      history.push('/users/create')
-    }}
-    users={useUsers(1, 99)}
-  />
+const UserSelectionContainer = ({ history }) => {
 
-const UserSelection = ({ onUserSelected, onCreateUser, users }) => {
+  const users = useUsers(1, 99)
+  const currentUser = useCurrentUser()
 
-  const [user, setUser] = useState('')
+  return (
+    <UserSelection
+      onUserSelected={user => {
+        if (user) {
+          history.push('/main')
+          setCurrentUser(user)
+        }
+      }}
+      onCreateUser={() => {
+        history.push('/users/create')
+      }}
+      users={users}
+      defaultUserId={currentUser ? currentUser.id : undefined}
+    />)
+}
+
+const UserSelection = ({ onUserSelected, onCreateUser, users, defaultUserId = '' }) => {
+
+  const [selectedUserId, setSelectedUserId] = useState(defaultUserId)
 
   return (
     <>
+
       <Typography style={{ marginBottom: 10 }} variant='h4' align='center'>Seleccionar Usuario</Typography>
+
       <FormControl style={{ marginBottom: 10 }} fullWidth>
         <InputLabel>Elija el usuario con el cual ingresar</InputLabel>
-        <Select value={user} onChange={e => setUser(e.target.value)}>
-          {users.map(user => <MenuItem key={user.id} value={user}>{user.name}</MenuItem>)}
+        <Select value={selectedUserId} onChange={e => setSelectedUserId(e.target.value)}>
+          {users.map(user => <MenuItem key={user.id} value={user.id}>{user.name}</MenuItem>)}
         </Select>
       </FormControl>
-      <Button style={{ width: '100%', marginBottom: 10 }} onClick={() => onUserSelected(user)} variant='contained' color='primary'>Ingresar</Button>
-      <Button style={{ width: '100%' }} onClick={onCreateUser} variant='contained' color='primary'>Crear Usuario</Button>
+
+      <Button
+        style={{ width: '100%', marginBottom: 10 }}
+        onClick={() => onUserSelected(users.find(user => user.id === selectedUserId))}
+        variant='contained'
+        color='primary'
+      >
+        Ingresar
+      </Button>
+
+      <Button
+        style={{ width: '100%' }}
+        onClick={onCreateUser}
+        variant='contained'
+        color='primary'
+      >
+        Crear Usuario
+      </Button>
+
     </>
   )
 }
